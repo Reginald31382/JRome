879bd2d75818e9bed785cd88bbd143c73e7cb010
Reggie <vassarjonez@yahoo.com>
2023-08-20 12:42:08 -0400

    added functionality to frontend auth

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -4,17 +4,15 @@ const notFound = (req, res, next) => {
   next(error);
 };
 
-const errorHandler = (req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
-  if (err.name === "CastError" && err.kind === "ObjectId") {
-    message = `Resource not found`;
-    statusCode = 404;
-  }
+  // NOTE: checking for invalid ObjectId moved to it's own middleware
+  // See README for further info.
 
   res.status(statusCode).json({
-    message,
+    message: message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
