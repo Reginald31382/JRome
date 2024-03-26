13406306da970442113725877127c9ee9ed312ce
Reginald J Dixon <vassarjonez@yahoo.com>
2024-03-26 01:34:49 -0400

    edited errors

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { BsArrowBarLeft } from "react-icons/bs";
